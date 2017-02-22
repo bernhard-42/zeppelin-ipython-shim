b2e37e5f1bdcf1f8a1f8626518fe0ba205a6aa62
bernhard <bernhard.walter@me.com>
2017-02-22 13:13:06 +0100

    listen to all ip adresses

--- websocket-server/websocketServer.py
+++ websocket-server/websocketServer.py
@@ -37,7 +37,7 @@ def message_received(client, server, message):
 			print("Wrong message")
 
 PORT=9001
-server = WebsocketServer(PORT)
+server = WebsocketServer(PORT, host="0.0.0.0")
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
