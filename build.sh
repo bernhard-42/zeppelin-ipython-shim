be66a646a3126751a4518a9270464cb0bc53df98
Bernhard Walter <bernhard.walter@me.com>
2017-02-26 12:07:40 +0100

    finally corrected a filename typo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ uglifyjs jupytershim/js-src/comm.js \
          jupytershim/js-src/notebook_comm.js \
          jupytershim/js-src/kernel.js \
          jupytershim/js-src/notebook.js \
-         -o jupytershim/js/jupyershim-min.js \
-         --source-map jupytershim/js/jupyershim-min.js.map \
+         -o jupytershim/js/jupytershim-min.js \
+         --source-map jupytershim/js/jupytershim-min.js.map \
          -p 1
 
